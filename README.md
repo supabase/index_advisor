de7da5244dbfbde6f3d5e9fa939df13858937061
Oliver Rice <github@oliverrice.com>
2023-02-13 13:56:16 -0600

    deterministic output

--- README.md
+++ README.md
@@ -2,9 +2,111 @@
 
 A PostgreSQL extension for recommending indexes to improve query performance.
 
+Features:
+- Supports generic parameters e.g. `$1`, `$2`
+- Supports materialized views
+- Identifies tables/columns obfuscaed by views
+
+
+## API
+
+### index_advisor(query text) returns table( index_statement text )
+
+#### Description
+For a given *query*, searches for a set of SQL DDL `create index` statements that improve the query's execution time;
+
+#### Signature
+```sql
+index_advisor(
+    query text
+)
+    returns table( index_statement text )
+    volatile
+    language plpgsql
+```
+
+## Usage
+
+For a minimal example, the `index_advisor` function can be given a single table query with a filter on an unindexed column.
+
+```sql
+create extension index_advisor cascade;
+
+create table if not exists public.book(
+    id int,
+    name text
+);
+
+select
+    index_advisor($$ select * from book where id = $1 $$);
+
+                index_advisor
+----------------------------------------------
+ CREATE INDEX ON public.book USING btree (id)
+```
+
+More complex queries may generate additional suggested indexes
+
+```sql
+create extension index_advisor;
+
+create table author(
+	id serial primary key,
+	name text not null
+);
+
+create table publisher(
+	id serial primary key,
+	name text not null,
+	corporate_address text
+);
+
+create table book(
+	id serial primary key,
+	author_id int not null references author(id),
+	publisher_id int not null references publisher(id),
+	title text
+);
+
+create table review(
+	id serial primary key,
+	book_id int references book(id),
+	body text not null
+);
+
+select
+	index_advisor('
+		select
+			book.id,
+			book.title,
+			publisher.name as publisher_name,
+			author.name as author_name,
+			review.body review_body
+		from
+			book
+			join publisher
+				on book.publisher_id = publisher.id
+			join author
+				on book.author_id = author.id
+			join review
+				on book.id = review.book_id
+		where
+			author.id = $1
+			and publisher.id = $2
+	');
+
+                     index_advisor                      
+--------------------------------------------------------
+ CREATE INDEX ON public.review USING btree (book_id)
+ CREATE INDEX ON public.book USING btree (author_id)
+ CREATE INDEX ON public.book USING btree (publisher_id)
+(3 rows)
+```
+
+
 ## Install
 
-Requires Postgres with [hypopg](https://github.com/HypoPG/hypopg) installed. 
+Requires Postgres with [hypopg](https://github.com/HypoPG/hypopg) installed.
 
 ```sh
 git clone https://github.com/supabase/index_advisor.git
@@ -15,6 +117,5 @@ sudo make install
 ## Run Tests
 
 ```sh
-PGHOST=localhost make install; PGHOST=localhost make installcheck 
+make install; make installcheck
 ```
-
--- index_advisor--0.1.0.sql
+++ index_advisor--0.1.0.sql
@@ -1,15 +1,15 @@
 create function index_advisor(
     query text
 )
-    returns table( index_staement text )
+    returns table( index_statement text )
     volatile
     language plpgsql
     as $$
 declare
     n_args int;
     prepared_statement_name text = 'index_advisor_working_statement';
-	hypopg_schema_name text = (select extnamespace::regnamespace::text from pg_extension where extname = 'hypopg');
-	explain_plan_statement text;
+    hypopg_schema_name text = (select extnamespace::regnamespace::text from pg_extension where extname = 'hypopg');
+    explain_plan_statement text;
     rec record;
     plan_initial jsonb;
     plan_final jsonb;
@@ -23,7 +23,7 @@ begin
 
         -- Create a prepared statement for the given query
         execute format('prepare %I as %s', prepared_statement_name, query);
-        
+
         -- Detect how many arguments are present in the prepared statement
         n_args = (
             select
@@ -50,62 +50,62 @@ begin
         );
 
         -- Store the query plan before any new indexes
-        execute explain_plan_statement into plan_initial; 
+        execute explain_plan_statement into plan_initial;
 
         -- Create possible indexes
         for rec in (
-			with extension_regclass as (
-				select
-					distinct objid as oid
-				from
-					pg_depend
-				where
-					deptype = 'e'
-			)
-			select
-				pc.relnamespace::regnamespace::text as schema_name,
-				pc.relname as table_name,
-				pa.attname as column_name,
-				format(
-					'select %I.hypopg_create_index($i$create index on %I.%I(%I)$i$)',
-					hypopg_schema_name,
-					pc.relnamespace::regnamespace::text,
-					pc.relname,
-					pa.attname
-				) hypopg_statement
-			from
-				pg_catalog.pg_class pc
-				join pg_catalog.pg_attribute pa
-					on pc.oid = pa.attrelid
-				left join extension_regclass er
-					on pc.oid = er.oid
-				left join pg_index pi
-					on pc.oid = pi.indrelid
-					and (select array_agg(x) from unnest(pi.indkey) v(x)) = array[pa.attnum]
-					and pi.indexprs is null -- ignore expression indexes
-					and pi.indpred is null -- ignore partial indexes
-			where
-				pc.relnamespace::regnamespace::text not in ( -- ignore schema list
-					'pg_catalog', 'pg_toast', 'information_schema'
-				)
-				and er.oid is null -- ignore entities owned by extensions
-				and pc.relkind in ('r', 'm') -- regular tables, and materialized views
-				and pc.relpersistence = 'p' -- permanent tables (not unlogged or temporary)
-				and pa.attnum > 0
-				and not pa.attisdropped
-				and pi.indrelid is null
+            with extension_regclass as (
+                select
+                    distinct objid as oid
+                from
+                    pg_depend
+                where
+                    deptype = 'e'
+            )
+            select
+                pc.relnamespace::regnamespace::text as schema_name,
+                pc.relname as table_name,
+                pa.attname as column_name,
+                format(
+                    'select %I.hypopg_create_index($i$create index on %I.%I(%I)$i$)',
+                    hypopg_schema_name,
+                    pc.relnamespace::regnamespace::text,
+                    pc.relname,
+                    pa.attname
+                ) hypopg_statement
+            from
+                pg_catalog.pg_class pc
+                join pg_catalog.pg_attribute pa
+                    on pc.oid = pa.attrelid
+                left join extension_regclass er
+                    on pc.oid = er.oid
+                left join pg_index pi
+                    on pc.oid = pi.indrelid
+                    and (select array_agg(x) from unnest(pi.indkey) v(x)) = array[pa.attnum]
+                    and pi.indexprs is null -- ignore expression indexes
+                    and pi.indpred is null -- ignore partial indexes
+            where
+                pc.relnamespace::regnamespace::text not in ( -- ignore schema list
+                    'pg_catalog', 'pg_toast', 'information_schema'
+                )
+                and er.oid is null -- ignore entities owned by extensions
+                and pc.relkind in ('r', 'm') -- regular tables, and materialized views
+                and pc.relpersistence = 'p' -- permanent tables (not unlogged or temporary)
+                and pa.attnum > 0
+                and not pa.attisdropped
+                and pi.indrelid is null
             )
             loop
                 -- Create the hypothetical index
                 execute rec.hypopg_statement;
             end loop;
 
-	/*
+    /*
     for rec in select * from hypopg()
         loop
             raise notice '%', rec;
         end loop;
-	*/
+    */
 
     -- Create a prepared statement for the given query
     -- The original prepared statement MUST be dropped because its plan is cached
@@ -113,22 +113,22 @@ begin
     execute format('prepare %I as %s', prepared_statement_name, query);
 
     -- Store the query plan after new indexes
-    execute explain_plan_statement into plan_final; 
+    execute explain_plan_statement into plan_final;
 
     --raise notice '%', plan_final;
 
     -- Idenfity referenced indexes in new plan
-	execute format(
-		'select
-			coalesce(array_agg(hypopg_get_indexdef(indexrelid)), $i${}$i$::text[])
-		from
-			%I.hypopg()
-		where
-			%s ilike ($i$%%$i$ || indexname || $i$%%$i$)
-		',
-		hypopg_schema_name,
+    execute format(
+        'select
+            coalesce(array_agg(hypopg_get_indexdef(indexrelid) order by indrelid, indkey::text), $i${}$i$::text[])
+        from
+            %I.hypopg()
+        where
+            %s ilike ($i$%%$i$ || indexname || $i$%%$i$)
+        ',
+        hypopg_schema_name,
         quote_literal(plan_final)::text
-	) into statements;
+    ) into statements;
 
     -- Reset all hypothetical indexes
     perform hypopg_reset();
