265069332231e425b64b94c0892b756370692259
Oliver Rice <github@oliverrice.com>
2023-02-14 05:54:14 -0600

    deallocate before run

--- index_advisor--0.1.0.sql
+++ index_advisor--0.1.0.sql
@@ -16,89 +16,90 @@ declare
     statements text[] = '{}';
 begin
 
-        -- Disallow multiple statements
-        if query ilike '%;%' then
-            raise exception 'query must not contain a semicolon';
-        end if;
+    -- Disallow multiple statements
+    if query ilike '%;%' then
+        raise exception 'query must not contain a semicolon';
+    end if;
 
-        -- Create a prepared statement for the given query
-        execute format('prepare %I as %s', prepared_statement_name, query);
+    -- Create a prepared statement for the given query
+    deallocate all;
+    execute format('prepare %I as %s', prepared_statement_name, query);
 
-        -- Detect how many arguments are present in the prepared statement
-        n_args = (
-            select
-                coalesce(array_length(parameter_types, 1), 0)
-            from
-                pg_prepared_statements
-            where
-                name = prepared_statement_name
-            limit
-                1
-        );
+    -- Detect how many arguments are present in the prepared statement
+    n_args = (
+        select
+            coalesce(array_length(parameter_types, 1), 0)
+        from
+            pg_prepared_statements
+        where
+            name = prepared_statement_name
+        limit
+            1
+    );
 
-        -- Create a SQL statement that can be executed to collect the explain plan
-        explain_plan_statement = format(
-            'set local plan_cache_mode = force_generic_plan; explain (format json) execute %I%s',
-            --'explain (format json) execute %I%s',
-            prepared_statement_name,
-            case
-                when n_args = 0 then ''
-                else format(
-                    '(%s)', array_to_string(array_fill('null'::text, array[n_args]), ',')
-                )
-            end
-        );
-
-        -- Store the query plan before any new indexes
-        execute explain_plan_statement into plan_initial;
-
-        -- Create possible indexes
-        for rec in (
-            with extension_regclass as (
-                select
-                    distinct objid as oid
-                from
-                    pg_depend
-                where
-                    deptype = 'e'
+    -- Create a SQL statement that can be executed to collect the explain plan
+    explain_plan_statement = format(
+        'set local plan_cache_mode = force_generic_plan; explain (format json) execute %I%s',
+        --'explain (format json) execute %I%s',
+        prepared_statement_name,
+        case
+            when n_args = 0 then ''
+            else format(
+                '(%s)', array_to_string(array_fill('null'::text, array[n_args]), ',')
             )
+        end
+    );
+
+    -- Store the query plan before any new indexes
+    execute explain_plan_statement into plan_initial;
+
+    -- Create possible indexes
+    for rec in (
+        with extension_regclass as (
             select
-                pc.relnamespace::regnamespace::text as schema_name,
-                pc.relname as table_name,
-                pa.attname as column_name,
-                format(
-                    'select %I.hypopg_create_index($i$create index on %I.%I(%I)$i$)',
-                    hypopg_schema_name,
-                    pc.relnamespace::regnamespace::text,
-                    pc.relname,
-                    pa.attname
-                ) hypopg_statement
+                distinct objid as oid
             from
-                pg_catalog.pg_class pc
-                join pg_catalog.pg_attribute pa
-                    on pc.oid = pa.attrelid
-                left join extension_regclass er
-                    on pc.oid = er.oid
-                left join pg_index pi
-                    on pc.oid = pi.indrelid
-                    and (select array_agg(x) from unnest(pi.indkey) v(x)) = array[pa.attnum]
-                    and pi.indexprs is null -- ignore expression indexes
-                    and pi.indpred is null -- ignore partial indexes
+                pg_depend
             where
-                pc.relnamespace::regnamespace::text not in ( -- ignore schema list
-                    'pg_catalog', 'pg_toast', 'information_schema'
-                )
-                and er.oid is null -- ignore entities owned by extensions
-                and pc.relkind in ('r', 'm') -- regular tables, and materialized views
-                and pc.relpersistence = 'p' -- permanent tables (not unlogged or temporary)
-                and pa.attnum > 0
-                and not pa.attisdropped
-                and pi.indrelid is null
+                deptype = 'e'
+        )
+        select
+            pc.relnamespace::regnamespace::text as schema_name,
+            pc.relname as table_name,
+            pa.attname as column_name,
+            format(
+                'select %I.hypopg_create_index($i$create index on %I.%I(%I)$i$)',
+                hypopg_schema_name,
+                pc.relnamespace::regnamespace::text,
+                pc.relname,
+                pa.attname
+            ) hypopg_statement
+        from
+            pg_catalog.pg_class pc
+            join pg_catalog.pg_attribute pa
+                on pc.oid = pa.attrelid
+            left join extension_regclass er
+                on pc.oid = er.oid
+            left join pg_index pi
+                on pc.oid = pi.indrelid
+                and (select array_agg(x) from unnest(pi.indkey) v(x)) = array[pa.attnum]
+                and pi.indexprs is null -- ignore expression indexes
+                and pi.indpred is null -- ignore partial indexes
+        where
+            pc.relnamespace::regnamespace::text not in ( -- ignore schema list
+                'pg_catalog', 'pg_toast', 'information_schema'
             )
-            loop
-                -- Create the hypothetical index
-                execute rec.hypopg_statement;
-            end loop;
+            and er.oid is null -- ignore entities owned by extensions
+            and pc.relkind in ('r', 'm') -- regular tables, and materialized views
+            and pc.relpersistence = 'p' -- permanent tables (not unlogged or temporary)
+            and pa.attnum > 0
+            and not pa.attisdropped
+            and pi.indrelid is null
+        )
+        loop
+            -- Create the hypothetical index
+            execute rec.hypopg_statement;
+        end loop;
 
     /*
     for rec in select * from hypopg()
@@ -132,8 +133,9 @@ begin
 
     -- Reset all hypothetical indexes
     perform hypopg_reset();
-    -- Delete the prepared statement
-    perform format('deallocate %I', prepared_statement_name);
+
+    -- Reset prepared statements
+    deallocate all;
 
     return query select * from unnest(statements);
 
